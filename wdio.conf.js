97c85829991d5ccece11a627ba4ecb808c880bb6
Natalija <93840667+natalijapav@users.noreply.github.com>
2026-03-27 21:26:32 +0100

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,32 +1,23 @@
 exports.config = {
 
     runner: 'local',
-     specs: [
-        './src/specs/**/*.js'
-    ],
-    
+    specs: ['./src/specs/**/*.js'],
     exclude: [ ],
-    
     maxInstances: 1,
    
     capabilities: [
     {browserName:'chrome'},
-    {browserName: 'MicrosoftEdge'}
-    ],
+    {browserName: 'MicrosoftEdge'}],
 
-   
     logLevel: 'info',
     bail: 0,
-  baseUrl: 'https://www.saucedemo.com/',
+    baseUrl: 'https://www.saucedemo.com/',
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
     services:['visual'],
-
-  
     framework: 'mocha',
     
-  
     reporters: [['allure', {
         outputDir: 'allure-results',
         disableWebdriverStepsReporting: true,
